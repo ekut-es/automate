1af821688db9bf6a7cc14d424845e01ed5b5f6f4
Christoph Gerum <christoph.gerum@uni-tuebingen.de>
2019-12-17 16:26:17 +0100

    Add nohz full to default kernel options

--- examples/shell/kernel/run.sh
+++ examples/shell/kernel/run.sh
@@ -1,13 +1,17 @@
 #!/bin/bash -e
 
-BOARD=jetsontx2
-CONFIG_OPTIONS="CONFIG_BPF=y CONFIG_PROC_KCORE=y CONFIG_IKCONFIG=y CONFIG_IKCONFIG_PROC=y CONFIG_BPF_JIT=y CONFIG_HAVE_EBPF_JIT=y CONFIG_HW_PERF_EVENTS=y CONFIG_KEXEC=y CONFIG_KEXEC_CORE=y"
+BOARDS="jetsontx2 jetsonagx"
+
+CONFIG_OPTIONS="CONFIG_BPF=y CONFIG_PROC_KCORE=y CONFIG_IKCONFIG=y CONFIG_IKCONFIG_PROC=y CONFIG_BPF_JIT=y CONFIG_HAVE_EBPF_JIT=y CONFIG_HW_PERF_EVENTS=y CONFIG_KEXEC=y CONFIG_KEXEC_CORE=y CONFIG_NO_HZ_FULL=y"
 
 config=""
 for option in $CONFIG_OPTIONS; do
     config="${config} -c ${option}" 
 done
 
-echo "Compile and run for $BOARD"
-automate  kernel.configure $BOARD jailhouse $config
-automate  kernel.build $BOARD jailhouse
+
+for BOARD in $BOARDS; do
+    echo "Compile kernel for $BOARD"
+    automate  kernel.configure $BOARD default $config
+    automate  kernel.build $BOARD default
+done
