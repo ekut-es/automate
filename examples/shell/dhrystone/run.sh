751e8c6da73ff61032cd24ef973d715a7c0ecb4d
Christoph Gerum <christoph.gerum@uni-tuebingen.de>
2020-01-07 08:31:51 +0100

    Add dhrystone

---
+++ examples/shell/dhrystone/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+
+BOARDS=$(automate board.board-ids)
+
+
+for BOARD in $BOARDS; do
+    echo "Compile and run for $BOARD"
+    automate  compiler.compile $BOARD -f dhry_1.c -f dhry_2.c -o dhry
+    automate  board.lock       $BOARD 
+    automate  board.put        $BOARD build-$BOARD/dhry
+    automate  board.run        $BOARD ./dhry
+    automate  board.unlock     $BOARD 
+done
