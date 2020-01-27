aa4391b5e62069611550ef626346651eec538482
Christoph Gerum <christoph.gerum@uni-tuebingen.de>
2020-01-27 10:54:48 +0100

    Adopt examples to changed API

--- examples/shell/dhrystone/run.sh
+++ examples/shell/dhrystone/run.sh
@@ -7,7 +7,7 @@ for BOARD in $BOARDS; do
     echo "Compile and run for $BOARD"
     automate  compiler.compile $BOARD -f dhry_1.c -f dhry_2.c -o dhry
     automate  board.lock       $BOARD 
-    automate  board.put        $BOARD build-$BOARD/dhry
+    automate  board.put        $BOARD builds/$BOARD/dhry
     automate  board.run        $BOARD ./dhry
     automate  board.unlock     $BOARD 
 done
