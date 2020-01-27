144fbfe50e5309355949d39b75520175f4d693d8
Christoph Gerum <christoph.gerum@uni-tuebingen.de>
2020-01-27 13:29:21 +0100

    Add missing make.py for make tasks

---
+++ automate/tasks/make.py
@@ -0,0 +1,52 @@
+from fabric import task
+
+
+def _get_builder(c, board, *args, **kwargs):
+    board = c.board(board)
+    cc = board.compiler()
+    builder = cc.builder("make", *args, **kwargs)
+
+    return builder
+
+
+@task
+def configure(c, board, builddir="", srcdir="", prefix=""):
+    builder = _get_builder(
+        c, board, builddir=builddir, srcdir=srcdir, prefix=prefix
+    )
+    builder.configure(c)
+
+
+@task
+def build(c, board, builddir="", srcdir="", prefix=""):
+    builder = _get_builder(
+        c, board, builddir=builddir, srcdir=srcdir, prefix=prefix
+    )
+    builder.build(c)
+
+
+@task
+def install(c, board, builddir="", srcdir="", prefix=""):
+    builder = _get_builder(
+        c, board, builddir=builddir, srcdir=srcdir, prefix=prefix
+    )
+    builder.install(c)
+
+
+@task
+def clean(c, board, builddir="", srcdir="", prefix=""):
+    builder = _get_builder(
+        c, board, builddir=builddir, srcdir=srcdir, prefix=prefix
+    )
+    builder.clean(c)
+
+
+@task
+def deploy(c, board, builddir="", srcdir="", prefix=""):
+    builder = _get_builder(
+        c, board, builddir=builddir, srcdir=srcdir, prefix=prefix
+    )
+    builder.deploy(c)
+
+
+__all__ = ["configure", "build", "clean", "install", "deploy"]
