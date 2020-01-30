bf91d21f288bc87f7c5a9a91925dd811a9fb0f47
Christoph Gerum <christoph.gerum@uni-tuebingen.de>
2020-01-30 13:10:21 +0100

    Add cmake hello world

--- examples/shell/hello_cmake/run.sh
+++ examples/shell/hello_cmake/run.sh
@@ -5,11 +5,11 @@ BOARDS=$(automate board.board-ids)
 
 for BOARD in $BOARDS; do
     echo "Compile and run for $BOARD"
-    automate  cmake.configure $BOARD -s hello
-    automate  cmake.build     $BOARD -s hello
-    automate  cmake.install   $BOARD -s hello 
+    automate  cmake.configure $BOARD -s hello_cmake
+    automate  cmake.build     $BOARD -s hello_cmake
+    automate  cmake.install   $BOARD -s hello_cmake
     automate  board.lock      $BOARD 
-    automate  cmake.deploy    $BOARD -s hello
-    automate  board.run       $BOARD ./hello/bin/CMakeHelloWorld 
+    automate  cmake.deploy    $BOARD -s hello_cmake
+    automate  board.run       $BOARD ./hello_cmake/bin/CMakeHelloWorld 
     automate  board.unlock    $BOARD 
 done
