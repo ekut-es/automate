ef276d79c4a8b2045e1c375c853471c811ec48b6
Christoph Gerum <christoph.gerum@uni-tuebingen.de>
2020-02-10 12:46:19 +0100

    Fix hello_cmake

--- examples/shell/hello_cmake/run.sh
+++ examples/shell/hello_cmake/run.sh
@@ -7,11 +7,11 @@ rm -rf builds
 for BOARD in $BOARDS; do
     echo "Compile and run for $BOARD"
     automate  cmake.configure $BOARD -s hello_cmake 
-    automate  cmake.build     $BOARD -s hello_cmake
-    automate  cmake.install   $BOARD -s hello_cmake
+    automate  cmake.build     $BOARD 
+    automate  cmake.install   $BOARD
     automate  board.lock      $BOARD
     automate  board.run       $BOARD 'rm -rf ./hello_cmake/'
-    automate  cmake.deploy    $BOARD -s hello_cmake
+    automate  cmake.deploy    $BOARD 
     automate  board.run       $BOARD ./hello_cmake/bin/CMakeHelloWorld 
     automate  board.unlock    $BOARD 
 done
