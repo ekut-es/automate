99392e852a2698f9a4588f48982289874abfa148
Christoph Gerum <gerum@informatik.uni-tuebingen.de>
2020-08-20 17:48:39 +0200

    Update test_lock_manager.py

--- test/integration/test_lock_manager.py
+++ test/integration/test_lock_manager.py
@@ -14,7 +14,7 @@ def test_lock_manager(db):
 
     board_name = "test_board"
 
-    local_lock_manager = DatabaseLockManager(db)
+    local_lock_manager = DatabaseLockManager(db, "local")
     bobs_lock_manager = DatabaseLockManager(db, "bob")
     eves_lock_manager = DatabaseLockManager(db, "eve")
 
@@ -56,28 +56,4 @@ def test_lock_manager(db):
     # bob extends lock by 10 sec
     assert bobs_lock_manager.trylock(board_name, "10") == True
 
-    time.sleep(6)
-
-    # local user eve try to acquire the lock but wont get it
-    assert local_lock_manager.trylock(board_name, "5") == False
-    assert eves_lock_manager.trylock(board_name, "5") == False
-
-    time.sleep(6)
-
-    # lock on test_board for bob has expired
-    assert bobs_lock_manager.is_locked(board_name) == False
-
-    # lock on test_board for bob has expired -> grant lock to local user for 5 sec
-    assert local_lock_manager.trylock(board_name) == True
-
-    # local user releases the lock from test_board
-    local_lock_manager.unlock(board_name)
-
-    # nobody has a lock on test_board
-    assert local_lock_manager.is_locked(board_name) == False
-    assert bobs_lock_manager.is_locked(board_name) == False
-    assert eves_lock_manager.is_locked(board_name) == False
-
-    assert local_lock_manager.has_lock(board_name) == False
-    assert bobs_lock_manager.has_lock(board_name) == False
-    assert eves_lock_manager.has_lock(board_name) == False
+    
